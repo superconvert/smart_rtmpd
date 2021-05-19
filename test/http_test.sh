3e213a82d9a776f73d5a00dce56bc07f2ce8ae16
superconvert <48017165+superconvert@users.noreply.github.com>
2021-05-20 07:12:26 +0800

    Update http_test.sh

--- test/http_test.sh
+++ test/http_test.sh
@@ -1,5 +1,11 @@
+// ----------------------------------------------------------------
+// Download file test
+// ----------------------------------------------------------------
 linux http download file from smart_rtmpd
 ab -n 1000 -c 100 http://192.168.1.102:8080/vod/test.mp4
 
-postman upload file to smart_rtmpd
+// ----------------------------------------------------------------
+// Upload file test
+// ----------------------------------------------------------------
+You can use tool postman upload file to smart_rtmpd
 http://192.168.1.102:8080/vod/test
