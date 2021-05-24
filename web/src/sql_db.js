6b4414c3827556e2d7d9c3f828e1f33de83c9cbe
superconvert <48017165+superconvert@users.noreply.github.com>
2021-05-24 23:32:48 +0800

    Add files via upload

---
+++ web/src/sql_db.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var sqlite3 = require('sqlite3').verbose();
+ 
+var db = db || {};
+ 
+/// ´ò¿ª±í
+db.sqlitedb = function(file) {
+	db.db = new sqlite3.Database(file); 
+    db.exist = fs.existsSync(file);
+    if ( !db.exist ) {
+		fs.openSync(file, 'w');
+    };
+};
+ 
+/// print error
+db.print_error = function(err) {
+	console.log("error msg:" + err.message + " error no:" + errno);
+};
+ 
+/// create table
+db.sqlitedb.prototype.create_table = function(sql) {
+	db.db.serialize(function() {
+		db.db.run(sql, function(err) {
+			if ( null != err ) {
+                db.print_error(err);
+                return;
+            }
+        });
+    });
+};
+ 
+/// tilesData format; [[level, column, row, content], [level, column, row, content]]
+db.sqlitedb.prototype.insert = function(sql, objects) {
+	db.db.serialize(function() {
+		var stmt = db.db.prepare(sql);
+        for (var i = 0; i < objects.length; ++i) {
+            stmt.run(objects[i]);
+        }
+    
+        stmt.finalize();
+    });
+};
+ 
+/// query sql
+db.sqlitedb.prototype.query = function(sql, callback) {
+	db.db.all(sql, function(err, rows) {
+        if (null != err) {
+            db.print_error(err);
+            return;
+        }
+ 
+        if (callback ) {
+            callback(rows);
+        }
+    });
+};
+ 
+/// execute sql
+db.sqlitedb.prototype.execute = function(sql) {
+    db.db.run(sql, function(err) {
+        if (null != err) {
+            db.print_error(err);
+        }
+    });
+};
+ 
+/// close db
+db.sqlitedb.prototype.close = function() {
+    db.db.close();
+};
+
+/// export db
+exports.sqlitedb = db.sqlitedb;
