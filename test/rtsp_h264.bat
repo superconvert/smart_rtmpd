b5e0892a4023b1b33983745f0b8deaae0d80f537
superconvert <48017165+superconvert@users.noreply.github.com>
2023-01-29 22:16:21 +0800

    Delete test directory

--- test/rtsp_h264.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-d:
-cd d:\ffmpeg\bin
-rem ffmpeg -threads 2 -re -stream_loop -1 -i 33.mp4 -vcodec libx264 -acodec aac -f rtsp rtsp://192.168.1.102:8554/live/stream
-ffmpeg -re -stream_loop -1 -i 1.mp4 -vcodec libx264 -acodec aac -f rtsp rtsp://192.168.1.102:8554/live/stream
-
--- test/rtsp_h265_player.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-d:
-cd d:\ffmpeg\bin
-ffplay rtsp://192.168.1.102:8554/live/stream
